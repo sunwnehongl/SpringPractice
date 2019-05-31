9dc371a43baac862cdacea9ace3b14ef713dbe10
sunwnehongl <30740977+sunwnehongl@users.noreply.github.com>
2019-05-31 22:17:56 +0800

    更新说明：通过XM文件来配置Spring的bean 提 交人：sunwenhong

---
+++ src/test/java/om/swh/spring/bean/BeanConfigByXML.java
@@ -0,0 +1,30 @@
+package om.swh.spring.bean;
+
+import com.swh.spring.bean.comfig.scan.AutoScanByClassConfig;
+import com.swh.spring.bean.service.CompactDiscService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @Auther: swh
+ * @Date: 2019/5/31 22:07
+ * @Description:
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:spring-service.xml")
+public class BeanConfigByXML {
+    // 播放器服务
+    @Autowired
+    private CompactDiscService compactDiscService;
+
+    /**
+     * 播放器的播放方法的测试方法
+     */
+    @Test
+    public void play_Test(){
+        compactDiscService.play();
+    }
+}
